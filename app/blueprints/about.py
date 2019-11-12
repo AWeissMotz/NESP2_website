74bc20ed0dea77caf3aa6b478da511b5667427b5
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2019-11-12 17:36:46 +0100

    Require login

--- app/blueprints/about.py
+++ app/blueprints/about.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
-
+from flask_login import login_required
 bp = Blueprint('about', __name__)
 
 
 @bp.route('/about')
+@login_required
 def about():
     return render_template('about.html')
