83f06316f3bab2bc42563b17a1270d6d2358ad64
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2019-11-12 17:36:35 +0100

    Introduce auth login

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,20 @@
 import os
 
+# init SQLAlchemy so we can use it later in our models
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_required
 try:
     from blueprints import resources, about
 except ModuleNotFoundError:
     from .blueprints import resources, about
+
+
+db = SQLAlchemy()
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 def create_app(test_config=None):
@@ -16,7 +26,22 @@ def create_app(test_config=None):
     )
     app.config.from_mapping(
         SECRET_KEY='dev',
+        SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir, 'app.db'),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
+
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return User.query.get(int(user_id))
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -34,8 +59,11 @@ def create_app(test_config=None):
     # register blueprints (like views in django)
     app.register_blueprint(resources.bp)
     app.register_blueprint(about.bp)
+    import auth
+    app.register_blueprint(auth.bp)
 
     @app.route('/')
+    @login_required
     def landing():
         return render_template('landing/index.html')
 
